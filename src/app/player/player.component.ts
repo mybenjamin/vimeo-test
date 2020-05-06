bdb55e0c733f070ef0117b7ed40c27efa5f63716
Yusuf Benjamin <mughammad.benjamin@ioco.tech>
2020-05-06 23:02:55 +0200

    cleans

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -6,25 +6,23 @@ import Player from '@vimeo/player';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit, AfterViewInit {
-  @ViewChild('myPlayer') vimeo;
+export class PlayerComponent implements AfterViewInit {
+  @ViewChild('myPlayer') vimeo; // Reference for HTML Element
   videoPlayer: any;
+
   options = {
     url: 'https://vimeo.com/280815263',
     width: 800
   };
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
   ngAfterViewInit() {
     this.videoPlayer = new Player(this.vimeo.nativeElement, this.options);
+
+    // Declare what to do when play occurs
     this.videoPlayer.on('play', () => {
       console.log('Played the video');
     });
+
     console.log(this.vimeo.nativeElement);
   }
 
